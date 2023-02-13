0312851ac8ef04c302a2d9948d4c701219391eca
alxmart <alxmartinez@gmail.com>
2023-02-13 15:39:07 -0300

    Added code to generate the phrases.

--- app/src/main/java/com/luizafmartinez/frasesdodia/MainActivity.java
+++ app/src/main/java/com/luizafmartinez/frasesdodia/MainActivity.java
@@ -3,12 +3,41 @@ package com.luizafmartinez.frasesdodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+      // Atributo
+      String[] frases = {
+              "Frase 01",
+              "Frase 02",
+              "Frase 03"
+      };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void gerarFrase(View view) {
+
+
+    }
+
+    public void exibirTodas(View view) {
+        TextView texto = findViewById(R.id.textResultado);
+        String textoResultado = "";
+        for ( String frase: frases ) {
+           textoResultado = textoResultado + frase;
+        }
+        texto.setText(textoResultado);
+    }
+
+
+
+
+
+
 }
